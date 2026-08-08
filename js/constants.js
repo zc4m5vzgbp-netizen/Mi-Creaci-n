397c06d93cef3e7f465eb2bf586a03e2fdece768
zc4m5vzgbp-netizen <zc4m5vzgbp@privaterelay.appleid.com>
2026-08-07 21:34:59 -0500

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -2,5 +2,5 @@ export const API_BASE = 'https://finnhub.io/api/v1';
 export const AV_BASE = 'https://www.alphavantage.co/query';
 export const INDEX_SYMBOLS = ['SPY', 'QQQ'];
 export const DEFAULT_FINNHUB_KEY = 'd9o5b19r01qqbd5t0nl0d9o5b19r01qqbd5t0nlg';
-export const HISTORY_DAYS = 400;
+export const HISTORY_DAYS = 1000;
 export const CHART_DISPLAY_DAYS = 150;
